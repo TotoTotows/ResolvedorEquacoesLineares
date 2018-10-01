797909532155b293d1808b4430379329b0f97182
Henrique Toto <30930755+TotoTotows@users.noreply.github.com>
2018-10-01 10:12:57 -0300

    .

--- ResolvedorEquacoesLineares/sistemasLineares.c
+++ ResolvedorEquacoesLineares/sistemasLineares.c
@@ -1,13 +1,11 @@
 #include <stdio.h>
 
-void main()
-{
     int aux;
     char* nomeArquivo;
     FILE *file;
 
-////////////////////////////////////////////////////////////////
-
+void main()
+{
     // Pega o nome do arquivo a ser lido
     printf("Qual o nome do arquivo a ser lido ? ");
     scanf("%s", &nomeArquivo);
